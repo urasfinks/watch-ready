0c30286cd32b956de984392db63c0645e6107d89
sfinks <urasfinks@yandex.ru>
2023-11-24 20:05:43 +0300

    Изменены цвета стандартной темы

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:rjdu/global_settings.dart';
 import 'package:rjdu/rjdu.dart';
+import 'package:rjdu/theme_provider.dart';
 
 void main() async {
   GlobalSettings().setHost("http://watchready.ru");
   GlobalSettings().setWs("ws://watchready.ru");
+  ThemeProvider.setColor("#019ba1", "white", "#f27f1b", "white");
   runApp(await RjDu.runApp());
 }
