cfb4acce0561b1b9c512e695ae66756ced515f22
sfinks <urasfinks@yandex.ru>
2024-09-17 18:52:43 +0300

    Исправлены url + добавлены пример занятия

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,10 @@ import 'package:rjdu/rjdu.dart';
 import 'package:rjdu/theme_provider.dart';
 
 void main() async {
-  //GlobalSettings().setHost("http://192.168.0.15");
-  //GlobalSettings().setWs("ws://192.168.0.15");
-  GlobalSettings().setHost("http://watchready.ru");
-  GlobalSettings().setWs("ws://watchready.ru");
+  //GlobalSettings().setHost("http://192.168.0.14");
+  //GlobalSettings().setWs("ws://192.168.0.14");
+  GlobalSettings().setHost("http://watch-ready.ru");
+  GlobalSettings().setWs("ws://watch-ready.ru");
   ThemeProvider.setColor("#019ba1", "white", "#f27f1b", "white");
   runApp(await RjDu.runApp());
 }
